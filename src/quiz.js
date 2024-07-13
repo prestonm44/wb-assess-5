b5638600c9c26b05d45fef039c550b1194b968ff
prestonm44 <preston.mitchell4444@gmail.com>
2024-07-12 19:33:09 -0600

    part 1 complete

--- src/quiz.js
+++ src/quiz.js
@@ -23,41 +23,51 @@ export const problem01 = await client.query(`
 console.log('-- Problem 01 --');
 console.log(problem01?.rows);
 
-// // Problem 02
-// export const problem02 = await client.query(`
-//   SELECT id 
-//   FROM orders
-//     WHERE fname LIKE '%Elizabeth%' AND lname LIKE '%Crocker%'
-//     FROM customers
-// `);
-// // console.log('-- Problem 02 --');
-// // console.log(problem02?.rows);
+// Problem 02
+export const problem02 = await client.query(`
+  SELECT o.id 
+  FROM orders o
+  WHERE o.customer_id IN (
+    SELECT c.id  
+    FROM customers c
+    WHERE c.fname LIKE '%Elizabeth%' AND c.lname LIKE '%Crocker%'
+  )
+`);
 
 // Problem 03
 export const problem03 = await client.query(`
-SELECT COUNT (*)
-FROM orders
-WHERE processed = FALSE
+  SELECT SUM (num_cupcakes)
+  FROM orders
+  WHERE processed = FALSE
 `);
 
 
 // Problem 04
 export const problem04 = await client.query(`
-SELECT SUM (o.num_cupcakes), c.name
-FROM orders as o
-INNER JOIN cupcakes c ON o.cupcake_id = c.id
-GROUP BY c.Name;
+  SELECT c.name, SUM (o.num_cupcakes)
+  FROM cupcakes c
+  LEFT JOIN orders o ON c.id = o.cupcake_id
+  GROUP BY c.name
+  ORDER BY c.name
 `);
 
 
 // Problem 05
-export const problem05 = null;
-// console.log('-- Problem 05 --');
-// console.log(problem05?.rows);
+export const problem05 = await client.query(`
+  SELECT cust.email, SUM (o.num_cupcakes) as total
+  FROM orders o 
+  INNER JOIN customers cust on o.customer_id = cust.id
+  GROUP BY cust.email
+  ORDER BY total DESC
+`);
+
 
 // Problem 06
-export const problem06 = null;
-console.log('-- Problem 06 --');
-// console.log(problem06?.rows);
+export const problem06 = await client.query(`
+  SELECT DISTINCT fname, lname, email
+  FROM customers c
+  INNER JOIN orders o on c.id = o.customer_id
+  WHERE cupcake_id = 5 AND o.processed = TRUE
+`);
 
 client.end();
